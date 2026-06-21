b4ee98c7db13b4b31467e3093b64705f6d729a50
makwanahappy25881-dev <makwanahappy25881@gmail.com>
2026-06-22 02:47:13 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = "biodex-cache-v1";
+const filesToCache = ["./", "./index.html", "./manifest.json"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(filesToCache))); });
+self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))); });
